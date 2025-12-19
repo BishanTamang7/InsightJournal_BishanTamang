53de36d9f101c50e69274de046eb6b79a6913e50
Bishan Tamang <bishantamang2025@gmail.com>
2025-12-19 16:01:26 +0545

    Add project files.

---
+++ MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿using InsightJournal.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace InsightJournal
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+            // SQLite database path
+            var dbPath = Path.Combine(
+                FileSystem.AppDataDirectory,
+                "insightjournal.db");
+
+            Console.WriteLine("DB PATH: " + dbPath);
+
+            // EF Core connection
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite($"Filename={dbPath}"));
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
